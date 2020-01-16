213ec6f532438a13ef4b6bf08e0c3e5c18533d08
Scott Harlan <seharlan@gmail.com>
2020-01-15 17:59:50 -0800

    logs player choice

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
 import { checkResults, getRandomThrow } from './randomThrow.js';
 
+//dom elements
+const playButton = document.getElementById('lets-play-button');
+const winLoseText = document.getElementById('win-lose');
+const scoreText = document.getElementById('score');
+const resetButton = document.getElementById('reset-button');
+
+//state
+let wins = 0;
+let losses = 0;
+let draws = 0;
 
 
+function totalPlays() {
+    return wins + losses + draws;
+}
+
+function getResults() {
+    //set new state of choice
+    const userChoice = document.querySelector('input:checked');
+
+    //determine player choice
+    const playerThrow = userChoice.value;
+    console.log(playerThrow);
+
+    //import computer choice
+    //declare checkResults
+    //display results in dom
+    //update state elements
+    //update dom with results
+
+}
+playButton.addEventListener('click', getResults);
+
+function resetGame() {
+    //reset state elements
+    //update dom
+}
+resetButton.addEventListener('click', resetGame);
