49695f1fd41550136eefbde2159476baa270f76b
Scott Harlan <seharlan@gmail.com>
2020-01-15 19:18:18 -0800

    fully functioning no css

--- app.js
+++ app.js
@@ -5,15 +5,29 @@ const playButton = document.getElementById('lets-play-button');
 const winLoseText = document.getElementById('win-lose');
 const scoreText = document.getElementById('score');
 const resetButton = document.getElementById('reset-button');
+const resultField = document.getElementById('result-field');
 
 //state
 let wins = 0;
 let losses = 0;
 let draws = 0;
 
-
 function totalPlays() {
     return wins + losses + draws;
+}
+
+function useResults(result) {
+    if (result === 'Winner') {
+        wins++;
+    } else if (result === 'Loser') {
+        losses++;
+    } else {
+        draws++;
+    }
+    const percent = Math.round((wins / totalPlays()) * 100);
+    resultField.style.opacity = '1';
+    winLoseText.textContent = `We have a ${result}!`;
+    scoreText.textContent = `You've won ${wins} out of ${totalPlays()} games or ${percent}%`;
 }
 
 function getResults() {
@@ -30,8 +44,9 @@ function getResults() {
 
     //declare checkResults
     const newResults = checkResults(computerThrow, playerThrow);
-    console.log(newResults);;
+    console.log(newResults);
 
+    useResults(newResults);
     //display results in dom
     //update state elements
     //update dom with results
@@ -41,6 +56,11 @@ playButton.addEventListener('click', getResults);
 
 function resetGame() {
     //reset state elements
+    wins = 0;
+    losses = 0;
+    draws = 0;
+
     //update dom
+    resultField.style.opacity = '0';
 }
 resetButton.addEventListener('click', resetGame);
